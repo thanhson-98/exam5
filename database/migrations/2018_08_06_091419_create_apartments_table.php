c79249980814fcb6873e1be0805beae55e21cc9f
thjennhjenoxi <sondtd00591@fpt.edu.vn>
2018-08-06 17:14:54 +0700

    bai kiem tra

---
+++ database/migrations/2018_08_06_091419_create_apartments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('price');
+            $table->string('general_information');
+            $table->string('detail_information');
+            $table->string('thumbnail');
+            $table->string('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
---
+++ database/seeds/apartmentTableSeeder.php
@@ -0,0 +1,181 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class apartmentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('apartments')->truncate();
+        DB::table('apartments')->insert([
+
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://vinhomesparadise.villas/wp-content/uploads/sites/95/2015/10/chung-cu-vinhomes-paradise.png',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://www.duan247.net/wp-content/uploads/2016/06/t%E1%BB%95ng-th%E1%BB%83-chung-cu-lieu-giai-.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://www.must-vacations.com/wp-content/uploads/2016/12/hanoirealestate.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://kenhchungcuthudo.com/Images/images/chung%20c%C6%B0%20Vinhomes%20Melodia/chung-cu-vinhomes-melodia.png',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSQIuPB8rItfOeF1v4ofv4GUB9dBdzQvwI6-nUDFjsmxL7MOJM',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://bannhachungcuhn.com/wp-content/uploads/2017/03/Ph%E1%BB%91i-c%E1%BA%A3nh-Vinhomes-Melodia.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://chungcugardenia.com.vn/wp-content/uploads/2015/12/VH-Gardenia-1_edited1.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://vinhomesparadise.villas/wp-content/uploads/sites/95/2015/10/chung-cu-vinhomes-paradise.png',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://chungcuimperiagarden.com/wp-content/uploads/sites/145/2015/06/ph%E1%BB%91i-c%E1%BA%A3nh-v%C6%B0%E1%BB%9Dn-hoa-b%E1%BB%83-b%C6%A1i.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://4.bp.blogspot.com/-nCeDRT773VA/Vk9H-ClBD2I/AAAAAAAAJWY/Z936HGgkAAE/s1600/Vinhomes-THD.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'https://flchalongbays.com/wp-content/uploads/2017/07/5-1024x683.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://bietthuvinhomes365.com/wp-content/uploads/2016/05/dd-chung-cu-vinhomes-lieu-giai-metropolis-1.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://proviewland.vn/wp-content/uploads/2017/09/h%C3%ACnh-%E1%BA%A3nh-vinhomes-central-park-1024x656.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://media.bizwebmedia.net/Sites/53202/data/upload/anh%20du%20an/du%20an%20royal%20city/chung_cu_royal_city.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://banchungcusaigon.com/banchungcu_saigon_1.20141111163807825.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'https://vinhometranduyhung.files.wordpress.com/2015/08/chung_cu_vinhomes_tran_duy_hung_6-1.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://vinhomesparadise.villas/wp-content/uploads/sites/95/2015/10/chung-cu-vinhomes-paradise.png',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://vhsland.vn/wp-content/uploads/2016/10/Phoi-canh1.jpg',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://vinhomesparadise.villas/wp-content/uploads/sites/95/2015/10/chung-cu-vinhomes-paradise.png',
+            ],
+            [
+                'name'=>'Vinhome',
+                'address'=>'Long biên',
+                'price'=>'2.000.000',
+                'general_information'=>'The Luxury là quần thể căn hộ được Chủ đầu tư và đơn vị đối tác thiết kế cảnh quan EDSA hàng đầu đến từ Mỹ đặt rất nhiều tâm huyết và sự ưu ái với điểm nhấn thiết kế nổi bật',
+                'detail_information'=>'Chung cư sang trọng',
+                'thumbnail'=>'http://vinhomesparadise.villas/wp-content/uploads/sites/95/2015/10/chung-cu-vinhomes-paradise.png',
+            ]
+
+        ]);
+    }
+}
